24123454315dc1d7f0a4aa25b46ffe4bee265b18
Sunil <sunil.asbgroup@gmail.com>
2026-03-03 17:12:18 +0530

    Update HeroSliderRepository.java

--- src/main/java/com/shashwatsamarpan/backend/repository/HeroSliderRepository.java
+++ src/main/java/com/shashwatsamarpan/backend/repository/HeroSliderRepository.java
@@ -2,6 +2,9 @@ package com.shashwatsamarpan.backend.repository;
 
 import com.shashwatsamarpan.backend.model.HeroSlider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface HeroSliderRepository extends JpaRepository<HeroSlider, String> {
+    List<HeroSlider> findByActive(boolean active);
 }
+
