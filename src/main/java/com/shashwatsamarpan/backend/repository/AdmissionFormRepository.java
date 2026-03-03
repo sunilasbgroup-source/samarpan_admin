96d6fa1883df0c06a72625aec0273799c66e8854
Sunil <sunil.asbgroup@gmail.com>
2026-03-03 17:13:17 +0530

    Update AdmissionFormRepository.java

--- src/main/java/com/shashwatsamarpan/backend/repository/AdmissionFormRepository.java
+++ src/main/java/com/shashwatsamarpan/backend/repository/AdmissionFormRepository.java
@@ -2,6 +2,8 @@ package com.shashwatsamarpan.backend.repository;
 
 import com.shashwatsamarpan.backend.model.AdmissionForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AdmissionFormRepository extends JpaRepository<AdmissionForm, String> {
+    List<AdmissionForm> findByActive(boolean active);
 }
