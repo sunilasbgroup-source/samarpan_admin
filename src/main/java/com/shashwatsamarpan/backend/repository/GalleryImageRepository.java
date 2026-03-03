98902569b8eeee996e377e420c302521fdbb9b09
Sunil <sunil.asbgroup@gmail.com>
2026-03-03 17:14:00 +0530

    Update GalleryImageRepository.java

--- src/main/java/com/shashwatsamarpan/backend/repository/GalleryImageRepository.java
+++ src/main/java/com/shashwatsamarpan/backend/repository/GalleryImageRepository.java
@@ -2,6 +2,8 @@ package com.shashwatsamarpan.backend.repository;
 
 import com.shashwatsamarpan.backend.model.GalleryImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface GalleryImageRepository extends JpaRepository<GalleryImage, String> {
+    List<GalleryImage> findByCategory(String category);
 }
